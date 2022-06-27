62f47133311263d25d2e23b1f8dff3f3cd246974
Rebecca Douglas <rebeccacmdouglas@gmail.com>
2022-06-27 16:51:06 +0100

    included some comments & readme

---
+++ user-registration/README.md
@@ -0,0 +1,7 @@
+Backend follows MVC pattern using SpringBoot
+Restful API should be self documenting providing endpoints for CRUD operations against User
+UserServiceImpl orchestrates read/write to database using JPA repository
+Small config class needed to handle cors
+
+Run requirements: mySQL database, java jdk 18.
+Database scripts included would typically be implemented via flyway migration
--- user-registration/src/main/java/com/pp/services/UserService.java
+++ user-registration/src/main/java/com/pp/services/UserService.java
@@ -5,12 +5,30 @@ import com.pp.model.entity.User;
 import java.util.List;
 
 public interface UserService {
-
+    /**
+     * Creates new user
+     * @param user User to save
+     * @return saved user
+     */
     User createUser(User user);
 
+    /**
+     * Gets all users
+     * @return returns list of users
+     */
     List<User> getAllUsers();
 
+    /**
+     * Updates user
+     * @param id user id
+     * @param user updated user
+     */
     void updateUser(Long id, User user);
 
+    /**
+     * Get user by id
+     * @param id user id
+     * @return returns user
+     */
     User getUserById(Long id);
 }
